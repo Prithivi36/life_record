a25e5d3312ab5c9004b621eb535feda2d1e17804
Prithivi36 <prithivipappian@gmail.com>
2025-04-24 19:32:50 +0530

    life_record_backend

---
+++ src/main/java/com/life/life_record/LifeRecordApplication.java
@@ -0,0 +1,13 @@
+package com.life.life_record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/life/life_record/LifeRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.life.life_record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
