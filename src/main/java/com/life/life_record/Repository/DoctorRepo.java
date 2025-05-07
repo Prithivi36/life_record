94234ab3c34b5ea555502081bf447feb6a2f5222
Prithivi36 <prithivipappian@gmail.com>
2025-05-07 19:46:25 +0530

    Backend Updated - v2

--- src/main/java/com/life/life_record/Repository/DoctorRepo.java
+++
@@ -1,9 +0,0 @@
-package com.life.life_record.Repository;
-
-import com.life.life_record.Entity.DoctorBase;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DoctorRepo extends MongoRepository<DoctorBase,String> {
-}
